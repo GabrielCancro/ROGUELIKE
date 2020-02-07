2b33340d53b55fb4e9d705ded56cd349e6a18dba
Buitre-Slayer <saacmanjarrez@gmail.com>
2020-02-07 06:54:09 -0600

    The new death animation update

--- Nodes/DeathAnimation.gd
+++ Nodes/DeathAnimation.gd
@@ -1,23 +1,22 @@
 tool
 extends Node2D
+
 export var nextScene:PackedScene
 var playerDied:=false
 
 func _ready():
-	ThePlayerDied()
-
-func ThePlayerDied():
 	#LLamar a esta funcion si el jugador murio
 	#Esta funcincion hace las animaciones necesarias y llama a una nueva escena
+	$GameOverSound.play()
 	$AnimationPlayer.play("FadeScreen")
 	yield($AnimationPlayer,"animation_finished")
-	$CanvasLayer/Label.visible=true
-	$CanvasLayer/restarText.visible=true
 	playerDied=true
 	
-func _input(event):
-	#para cambiar de escena al precionar r
-	if Input.is_key_pressed(KEY_R) and playerDied: get_tree().change_scene_to(nextScene)
+func _process(delta):
+	#para cambiar de escena al precionar
+	if Input.is_action_just_pressed("ui_accept") and playerDied:
+		get_tree().change_scene_to(nextScene)
 	
-#func _get_configuration_warning() ->String:
-#	return "La propiedad Next Scene debe ser completada" if not nextScene else ""
+	
+func _get_configuration_warning() ->String:
+	return "La propiedad Next Scene debe ser completada" if not nextScene else ""
